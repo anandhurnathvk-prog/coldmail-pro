9696dc93d4877e286ff9d65e2dccce6a378e3af4
anandhurnathvk-prog <anandhurnathvk@gmail.com>
2026-04-18 23:44:41 +0530

    Delete api directory

--- api/key.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const key = process.env.GEMINI_API_KEY || '';
-  res.setHeader('Content-Type', 'application/javascript');
-  res.setHeader('Cache-Control', 'no-store');
-  res.status(200).send(`window.__GMKEY__ = "${key}";`);
-}
